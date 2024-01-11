3eb074132bbfed9d8541eefe0b56bc9cd5cdb81c
rahul <mrsvaz@icloud.com>
2024-01-11 13:47:16 +0530

    vikn codes machine test project created

---
+++ android/app/src/main/kotlin/com/example/vikncodes_machine_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vikncodes_machine_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RootWidget());
+}
+
+
+class RootWidget extends StatelessWidget {
+  const RootWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
